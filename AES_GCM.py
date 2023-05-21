ea69ebc64e2942cbb86f7875237db8c8cf0ff21f
Kazuma Nishimura <106137507+w4mally@users.noreply.github.com>
2023-05-22 01:11:23 +0900

    Add files via upload

--- AES_GCM.py
+++ AES_GCM.py
@@ -1,11 +1,10 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
-def AES_GCM(key,nonce,text){
+def AES_GCM(key,nonce,text):
     c = AES.new(key, AES.MODE_GCM, nonce)
     cipher, tag = c.encrypt_and_digest(text)
     return cipher, tag
-}
 
 key = get_random_bytes(16)
 nonce = get_random_bytes(12)
